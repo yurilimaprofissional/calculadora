a427624a7a73188a0385aedb33545987a62b05e6
Yuri Lima <yurilimaprofissional@gmail.com>
2025-06-03 18:25:57 -0300

    Update script.js

--- script.js
+++ script.js
@@ -12,3 +12,10 @@ function apagar() {
     var resultado = document.getElementById('resultado').innerHTML;
     document.getElementById('resultado').innerHTML = resultado.substring(0, resultado.length -1);
 }
+
+function calcular() {
+    var resultado = document.getElementById('resultado').innerHTML;
+    if(resultado) {
+        document.getElementById('resultado').innerHTML = eval(resultado)
+    }
+}
